ee18c369e4f8b26edd2a1f42d2f2fd04cc53fa8e
bala-amypo <bala@amypo.in>
2025-12-17 09:52:10 +0000

    User commit

--- src/main/java/com/example/demo/newservice/newserviceimpl/NewfileServiceImpl.java
+++ src/main/java/com/example/demo/newservice/newserviceimpl/NewfileServiceImpl.java
@@ -7,44 +7,43 @@ import org.springframework.stereotype.Service;
 import com.example.demo.newentity.NewfileEntity;
 import com.example.demo.newrepository.NewfileRepo;
 import com.example.demo.newservice.NewfileService;
-
 @Service
 public class NewfileServiceImpl implements NewfileService {
 
     private final NewfileRepo rep;
 
-    // Constructor Injection
-    public NewfileServiceImpl(NewfileRepo rep) {
-        this.rep = rep;
+    public NewfileServiceImpl(){
+        this.rep=rep;
     }
-
     @Override
     public NewfileEntity savedata(NewfileEntity newfile) {
         return rep.save(newfile);
+        
     }
-
-
-
-
 
     @Override
     public NewfileEntity getidval(Long id) {
         return rep.findById(id);
+        
     }
 
     @Override
     public List<NewfileEntity> getall() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getall'");
+        return rep.findAll();
+       
     }
 
     @Override
     public NewfileEntity update(Long id, NewfileEntity newfile) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        NewfileEntity existing =getidval(id);
+        existing.setName(newfile.getName());
+        existing.setEmail(newfile.getEmail());
+        return rep.save(existing);
+       
     }
     @Override
 public void delete(Long id) {
+    delete(Long id);
     
 }
 
